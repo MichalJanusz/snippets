5ace62d51fa21086e496ab340ad191d1ad98ad20
MichalJanusz <janusz.j.michal@gmail.com>
2021-04-20 12:11:14 +0200

    section 8 started

---
+++ intermediate_python_pl/section 8/lab_1.py
@@ -0,0 +1,42 @@
+import requests
+import os
+import shutil
+
+
+def save_url_to_file(url, file_path):
+    r = requests.get(url, stream=True)
+    with open(file_path, "wb") as f:
+        f.write(r.content)
+
+
+url = 'http://www.mobilo24.eu/spis/'
+dir = 'lab_1/'
+tmpfile = 'download.tmp'
+file = 'spis.html'
+
+tmpfile_path = os.path.join(dir, tmpfile)
+file_path = os.path.join(dir, file)
+
+try:
+    if os.path.exists(tmpfile_path):
+        print(f'Removing {tmpfile_path}')
+        os.remove(tmpfile_path)
+
+    print(f'Downloading url {url}')
+    save_url_to_file(url, tmpfile_path)
+
+    print(f'Copying file {tmpfile_path} {file_path}')
+
+    shutil.copy(tmpfile_path, file_path)
+except Exception as e:
+    print(f'Error downloading URL {url}')
+    print(f'Error details: {e}')
+else:
+    print(f'URL Downloaded successfully {file}')
+
+finally:
+    if os.path.exists(tmpfile_path):
+        print(f'Final removal of the file {tmpfile_path}')
+
+        os.remove(tmpfile_path)
+    print('Done')
